92e013936f14ec9118096f01ba5d01fe8f3ba100
AshishKumar409 <ashishkumarnath9@gmail.com>
2021-06-24 16:10:37 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 let vibrator = document.getElementById("vibrate")
 
 navigator.vibrate = navigator.vibrate||navigator.webkitVibrate||navigator.mozVibrate||navigator.msVibrate
-console.log("hello")
+
 if(navigator.vibrate){
    vibrator.addEventListener("click",()=>{
-     navigator.vibrate(2000)  
-     console.log("It is working") 
-     console.log("brrrrrrr");
+     navigator.vibrate(1000)  
    })
 }
