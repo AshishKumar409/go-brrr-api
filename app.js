ce4832d0f04fb2c53c560141a15a4a70052d8e20
AshishKumar409 <ashishkumarnath9@gmail.com>
2021-06-24 16:41:13 +0530

    done maybe

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 let vibrator = document.getElementById("vibrate")
+let darthVader = document.getElementById("DarthVader")
+let jamesBond = document.getElementById("JamesBond")
+let superMario= document.getElementById("SuperMario")
 
 navigator.vibrate = navigator.vibrate||navigator.webkitVibrate||navigator.mozVibrate||navigator.msVibrate
 
 if(navigator.vibrate){
-   vibrator.addEventListener("click",()=>{
-     navigator.vibrate(1000)  
+   [vibrator,darthVader,jamesBond,superMario].forEach((val)=>{
+     val.addEventListener("click",(ev)=>{
+     if(ev.target.id==="vibrate"){
+         navigator.vibrate(1000) 
+     }else if(ev.target.id==="DarthVader"){
+      navigator.vibrate([500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40, 500]);
+     }else if(ev.target.id==="SuperMario"){
+       navigator.vibrate([125,75,125,275,200,275,125,75,125,275,200,600,200,600]);
+     }else {
+       navigator.vibrate([200,100,200,275,425,100,200,100,200,275,425,100,75,25, 75,125,75,25,75,125,100,100]);
+     }
+     }) 
    })
 }
